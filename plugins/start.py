f8b04c7dcfa1edab6006f2eba129138ffcbae1ff
ehabrino22 <98663001+ehabrino22@users.noreply.github.com>
2022-02-04 19:39:36 +0200

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -3,15 +3,13 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 
 @Client.on_message(Filters.command(["start"]), group=-2)
 async def start(client, message):
-# I Can See You !!
-    do = requests.get(
-        f"https://api.telegram.org/bot2087689939:AAFpKyW4R-UzwtR8sF0wp_DnKyxaStJyxQ8/getChatMember?chat_id=@animeeven&user_id={message.from_user.id}").text
-    if do.count("left") or do.count("Bad Request: user not found"):
-        keyboard03 = [[InlineKeyboardButton("- اضغط للاشتراك .", url='https://t.me/animeeven')]]
-        reply_markup03 = InlineKeyboardMarkup(keyboard03)
-        await message.reply_text('- اشترك بقناة البوت لتستطيع تشغيل الاغاني  .',
-                                 reply_markup=reply_markup03)
-    else:
+                         
+                                , parse_mode='markdown', reply_markup=markup_help)
+            else:
+                bot.send_message(message.chat.id, text=f'*اشترك(@{ch}) واضغط (/start)*', parse_mode='markdown')
+        else:
+            bot.send_message(message.chat.id, text=f'*اشترك (@{req_channel}) واضغط (/start)*', parse_mode='markdown')
+
     # return
     joinButton = InlineKeyboardMarkup([
         [InlineKeyboardButton("Channel", url="https://t.me/aryan_bots")],
